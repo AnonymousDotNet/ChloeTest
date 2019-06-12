50ea265609507900a6476495d84aaea3724427b3
dell <dell@DESKTOP-MIRKI0O>
2019-06-12 09:57:21 +0800

    First submission

---
+++ ChloeTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChloeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Test.DB/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Test.DB
+{
+    public class Class1
+    {
+    }
+}
